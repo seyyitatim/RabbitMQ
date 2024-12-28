00211878c21761f5c61e9e4829e6123e86707971
seyyitatim <seyyitatim@gmail.com>
2024-12-28 15:46:49 +0300

    added rabbitmq project and notes

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,181 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+await TopicExchange();
+
+static async Task BasicConsumer()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    // queue olusturma
+    // consumer'daki tanimlama publisher ile ayni olmalidir
+    var queueName = "example-queue";
+    await channel.QueueDeclareAsync(queue: queueName, exclusive: false, autoDelete: false, durable: true);
+    // ayni anda kac mesaji isleyecegi bilgisini tanimladigimiz yer
+    // bu ayar yapilmazsa tum mesajlar ilk baglanan consumer a gider 
+    await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+
+    // queue'dan mesaj okuma
+    AsyncEventingBasicConsumer consumer = new(channel);
+
+    // autoAck: queue'dan alinan mesajin basarili yanitini almadan silinmemesi saglar. Bu ayar consumerda yapilir.
+    await channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer);
+
+    consumer.ReceivedAsync += async (sender, e) =>
+    {
+        Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+        Task.Delay(1000).Wait();
+        // multiple: sadece ilgili mesaja dair onay bilgisini yollar. Aksi takdirde bundan oncekiler icinde onaylar
+        await channel.BasicAckAsync(deliveryTag: e.DeliveryTag, multiple: false);
+
+        // islenmeyen mesaji geri gonderme
+        // requeue: tekrardan kuyruga eklenmesi isteniyorsa eklenir
+        //await channel.BasicNackAsync(deliveryTag: e.DeliveryTag, multiple: false, requeue: true);
+
+    };
+
+
+    // bir kuyruktaki tum mesajlarin islenmesini reddetme
+    //var consumerTag = await channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer);
+    //await channel.BasicCancelAsync(consumerTag);
+
+    // bir kuyruktaki tek bir mesajin islenmesini reddetme
+    //var consumerTag = await channel.BasicConsumeAsync(queue: queueName, autoAck: false, consumer);
+    //await channel.BasicRejectAsync(deliveryTag: e.DeliveryTag, requeue: true);
+
+    Console.Read();
+}
+
+static async Task DirectExchange()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    // 1.adim
+    await channel.ExchangeDeclareAsync(
+        exchange: "direct-exchange-example",
+        type: ExchangeType.Direct);
+
+    // 2.adim
+    var queueName = channel.QueueDeclareAsync().Result.QueueName;
+
+    // 3.adim
+    await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "direct-exchange-example",
+        routingKey: "direct-queue-example");
+
+    AsyncEventingBasicConsumer consumer = new(channel);
+
+    await channel.BasicConsumeAsync(
+        queue: queueName,
+        autoAck: true,
+        consumer: consumer);
+
+    consumer.ReceivedAsync += async (sender, e) =>
+    {
+        string message = Encoding.UTF8.GetString(e.Body.Span);
+        Console.WriteLine(message);
+
+        Task.Delay(1000).Wait();
+    };
+
+
+    Console.Read();
+
+    // 1.Adim publisher'da ki exchange ile birebir ayni isim ve type'a sahip bir exchange tanimlanmalidir
+    // 2.adim publisher tarafindan routing key'de bulunan degerdeki kuyruga gonderilen mesajlari kendi olusturdugumuz kuyruga yonlendirerek tuketmemiz gerekmektedir. Bunun icin oncelikli bir kuyruk olusturulmalidir
+
+}
+
+static async Task FanoutExchange()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "fanout-exchange-example",
+        type: ExchangeType.Fanout);
+
+    Console.Write("Kuyruk adini giriniz: ");
+    var queueName = Console.ReadLine();
+    await channel.QueueDeclareAsync(
+        queue: queueName,
+        exclusive: false);
+
+    await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "fanout-exchange-example",
+        routingKey: string.Empty);
+
+    AsyncEventingBasicConsumer consumer = new(channel);
+
+    await channel.BasicConsumeAsync(
+        queue: queueName,
+        autoAck: true, // message acknowledge
+        consumer: consumer);
+
+    consumer.ReceivedAsync += async (sender, e) =>
+    {
+        string message = Encoding.UTF8.GetString(e.Body.Span);
+        Console.WriteLine(message);
+
+        Task.Delay(1000).Wait();
+    };
+
+
+    Console.Read();
+}
+
+static async Task TopicExchange()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "topic-exchange-example",
+        type: ExchangeType.Topic);
+
+    Console.Write("Dinlenecek topic formatini giriniz: ");
+    var topic = Console.ReadLine();
+    var queueName = channel.QueueDeclareAsync().Result.QueueName;
+
+    await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "topic-exchange-example",
+        routingKey: topic);
+
+    AsyncEventingBasicConsumer consumer = new(channel);
+
+    await channel.BasicConsumeAsync(
+        queue: queueName,
+        autoAck: true, // message acknowledge
+        consumer: consumer);
+
+    consumer.ReceivedAsync += async (sender, e) =>
+    {
+        string message = Encoding.UTF8.GetString(e.Body.Span);
+        Console.WriteLine(message);
+
+        Task.Delay(1000).Wait();
+    };
+
+
+    Console.Read();
+}
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,176 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+await HeaderExchange();
+
+async Task BasicPublisher()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    // queue olusturma
+    // exclusive: tum icerik tuketildiginde kuyrugun silinip silinmemesini belirttigimiz kisim
+    // durable: mesajinlarin kalici mi bilgisini belirttigimiz kisim
+    await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false, autoDelete: false, durable: true);
+
+
+
+    // queue'ya mesaj gonderilmesi
+    //for (int i = 0; i < 100; i++)
+    //{
+    //    var message = Encoding.UTF8.GetBytes("merhaba " + i);
+    //    await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message);
+    //}
+
+    Console.WriteLine("mesaj gonderildi");
+    Console.Read();
+}
+
+// mesajlarin islenmeden silinmemesi
+async Task DurabilityPublisher()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    // queue olusturma
+    // exclusive: tum icerik tuketildiginde kuyrugun silinip silinmemesini belirttigimiz kisim
+    // durable: mesajinlarin kalici mi bilgisini belirttigimiz kisim - ayrica mesaj icinde bu tanimi yapmak lazim
+    await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false, autoDelete: false, durable: true);
+
+    var properties = new BasicProperties()
+    {
+        Persistent = true,
+    };
+
+    // queue'ya mesaj gonderilmesi
+    for (int i = 0; i < 100; i++)
+    {
+        var message = Encoding.UTF8.GetBytes("merhaba " + i);
+        await channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: message, basicProperties: properties, mandatory: true);
+    }
+
+    Console.WriteLine("mesaj gonderildi");
+    Console.Read();
+}
+
+// Direct Exchange
+// exchange bagli kuyruktan ilgili routinge gore tum consumerlara mesaj iletme
+async Task DirectExchange()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    await channel.ExchangeDeclareAsync(exchange: "direct-exchange-example", type: ExchangeType.Direct);
+
+    while (true)
+    {
+        Console.Write("Mesaj: ");
+        string message = Console.ReadLine();
+        var byteMessage = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync(
+            exchange: "direct-exchange-example",
+            routingKey: "direct-queue-example",
+            body: byteMessage);
+
+        Console.WriteLine("mesaj gonderildi");
+    }
+}
+
+// Fanout Exchange
+// exchange e bagli tum kuyruklara mesaj gonderme
+async Task FanoutExchange()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "fanout-exchange-example", 
+        type: ExchangeType.Fanout);
+
+    while (true)
+    {
+        Console.Write("Mesaj: ");
+        string message = Console.ReadLine();
+        var byteMessage = Encoding.UTF8.GetBytes(message);
+
+        await channel.BasicPublishAsync(
+            exchange: "fanout-exchange-example",
+            routingKey: string.Empty,
+            body: byteMessage);
+
+        Console.WriteLine("mesaj gonderildi");
+    }
+}
+
+// Topic Exchange
+// gonderilen formatla eslesen tum kuyruklara mesaj gonderme
+// * tek bir kelime
+// # birden fazla kelime
+// *.log.# logdan once bir kelime logdan sonra istenildigi kadar alan gelebilir
+async Task TopicExchange()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "topic-exchange-example",
+        type: ExchangeType.Topic);
+
+    while (true)
+    {
+        Console.Write("topic belirtiniz: ");
+        string topic = Console.ReadLine();
+        var byteMessage = Encoding.UTF8.GetBytes("hello world");
+
+        await channel.BasicPublishAsync(
+            exchange: "topic-exchange-example",
+            routingKey: topic,
+            body: byteMessage);
+
+        Console.WriteLine("mesaj gonderildi");
+    }
+}
+
+// Header Exchange
+async Task HeaderExchange()
+{
+    // baglanti olusturma
+    ConnectionFactory factory = new();
+    factory.Uri = new("amqps://cbnrbnmg:V89R6sjHbaxQ60a35enaOAHUe0QV5Ywe@sparrow.rmq.cloudamqp.com/cbnrbnmg");
+    var connection = await factory.CreateConnectionAsync();
+    var channel = await connection.CreateChannelAsync();
+
+    await channel.ExchangeDeclareAsync(
+        exchange: "topic-exchange-example",
+        type: ExchangeType.Topic);
+
+    while (true)
+    {
+        Console.Write("topic belirtiniz: ");
+        string topic = Console.ReadLine();
+        var byteMessage = Encoding.UTF8.GetBytes("hello world");
+
+        await channel.BasicPublishAsync(
+            exchange: "topic-exchange-example",
+            routingKey: topic,
+            body: byteMessage);
+
+        Console.WriteLine("mesaj gonderildi");
+    }
+}
